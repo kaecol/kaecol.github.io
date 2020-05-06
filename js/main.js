8489963db203ed853ca9b8f9513e4b0d66968d38
kaecol <kaecol@users.noreply.github.com>
2020-05-06 16:39:06 +0800

    alumn iindex js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $("#anythingSearch").keypress(function(event) {
             if (event.keyCode === 13) {
-                $("#toggleAccordions-show").click(); 
+                $("#toggleAccordions-show").click();
             }
         });
 
@@ -347,19 +347,19 @@ jQuery(document).ready(function($) {
 	siteSticky();
 
 	// navigation
+
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
-   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
 
-      var hash = this.hash;
+   	$("body").on("click", ".main-menu li a[href^='#'] a[href^=''], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
 
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 600, 'easeInOutExpo', function(){
-        window.location.hash = hash;
-      });
+      var dog = this.hash;
 
+        $('html, body').animate({
+          'scrollTop': $(dog).offset().top
+        }, 600, 'easeInOutExpo', function(){
+          window.location.hash = dog;
+        });
     });
   };
   OnePageNavigation();
